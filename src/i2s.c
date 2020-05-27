0d29d2171e4d90cf392b34411127500617193c25
bunnie <bunnie@kosagi.com>
2020-05-27 16:22:42 +0800

    LRCK haxxx

--- src/i2s.c
+++ src/i2s.c
@@ -7,6 +7,7 @@
 #include "nrf.h"
 #include "nrfx.h"
 #include "nrfx_i2s.h"
+#include "nrfx_pwm.h"
 
 #include "i2s.h"
 
@@ -14,10 +15,13 @@ static volatile uintptr_t buffers[2];
 static volatile size_t buffer_size;
 static volatile uint8_t buffer_num;
 
+static uint16_t lrck_duty_cycles[PWM0_CH_NUM] = { 32 };
+
 void nus_init(const struct i2s_pin_config *cfg, void *buffer, size_t len) {
-    NRF_I2S->PSEL.MCK = cfg->bit_clock_pin_number;
-    NRF_I2S->PSEL.LRCK = cfg->word_select_pin_number;
-    NRF_I2S->PSEL.SCK = (0 + 11); // Assign an unused pin
+    NRF_I2S->PSEL.MCK = (0 + 11); // Assign an unused pin
+    //    NRF_I2S->PSEL.LRCK = cfg->word_select_pin_number;
+    NRF_I2S->PSEL.LRCK = (0 + 24);
+    NRF_I2S->PSEL.SCK = cfg->bit_clock_pin_number;
     NRF_I2S->PSEL.SDOUT = 0xFFFFFFFF;
     NRF_I2S->PSEL.SDIN = cfg->data_pin_number;
 
@@ -26,7 +30,7 @@ void nus_init(const struct i2s_pin_config *cfg, void *buffer, size_t len) {
     NRF_I2S->CONFIG.TXEN = I2S_CONFIG_TXEN_TXEN_Disabled;
     NRF_I2S->CONFIG.MCKEN = I2S_CONFIG_MCKEN_MCKEN_Enabled;
     NRF_I2S->CONFIG.SWIDTH = I2S_CONFIG_SWIDTH_SWIDTH_16Bit;
-    NRF_I2S->CONFIG.RATIO = I2S_CONFIG_RATIO_RATIO_64X;
+    NRF_I2S->CONFIG.RATIO = I2S_CONFIG_RATIO_RATIO_32X;
     NRF_I2S->CONFIG.MCKFREQ = I2S_CONFIG_MCKFREQ_MCKFREQ_32MDIV8;
     NRF_I2S->CONFIG.CHANNELS = I2S_CONFIG_CHANNELS_CHANNELS_Left;
 
@@ -37,6 +41,28 @@ void nus_init(const struct i2s_pin_config *cfg, void *buffer, size_t len) {
     buffers[1] = ((uintptr_t)buffer) + len / 2;
     buffer_size = len / 2;
     buffer_num = 0;
+
+    NRF_PWM_Type* pwm    = NRF_PWM0;
+    
+    pwm->ENABLE = 0;
+
+    pwm->PSEL.OUT[0] = cfg->word_select_pin_number;
+    
+    pwm->MODE            = PWM_MODE_UPDOWN_Up;
+    pwm->COUNTERTOP      = 64;
+    pwm->PRESCALER       = PWM_PRESCALER_PRESCALER_DIV_4; // 4 MHz, same as I2S
+    pwm->DECODER         = PWM_DECODER_LOAD_Individual;
+    pwm->LOOP            = 0;
+  
+    pwm->SEQ[0].PTR      = (uint32_t) (lrck_duty_cycles);
+    pwm->SEQ[0].CNT      = 4; // default mode is Individual --> count must be 4
+    pwm->SEQ[0].REFRESH  = 0;
+    pwm->SEQ[0].ENDDELAY = 0;
+    
+    pwm->ENABLE = 1;
+    
+    pwm->EVENTS_SEQEND[0] = 0;
+  
 }
 
 void nus_start(void) {
@@ -53,11 +79,11 @@ void nus_start(void) {
     // Turn on the interrupt to the NVIC but not within the NVIC itself. This
     // will wake the CPU and keep it awake until it is serviced without
     // triggering an interrupt handler.
+    NRF_PWM0->TASKS_SEQSTART[0] = 1;
     NRF_I2S->INTENSET = I2S_INTENSET_RXPTRUPD_Msk;
     NRF_I2S->ENABLE = I2S_ENABLE_ENABLE_Enabled;
 
     NRF_I2S->TASKS_START = 1;
-
     return;
 }
 
